24a34a3e52b43f0e3c587a25f5e95fb9c7323153
devshawn <musicalshawn@gmail.com>
2014-11-21 11:32:02 -0600

    Update Project.rkt
    Work done in class on 11/21/14

--- Project.rkt
+++ Project.rkt
@@ -4,40 +4,97 @@
 ; Game Project
 ; Shawn Seymour & Zach Litzinger
 
+; Constants
 (define width 400)
 (define height 300)
-(define speed 5)
+(define speed 10)
+(define bullet-speed 7)
+(define enemyspeed 5)
+(define spawn-speed .2)
 (define gravity 5)
 (define blank-scene (empty-scene width height))
 
+(define bullet-img (rectangle 5 15 "solid" "red"))
+(define enemyimg (rectangle 20 15 "solid" "green"))
 (define-struct player [x y img scale])
-(define player-1 (make-player 200 150 (circle 10 "solid" "red") 2))
+(define-struct enemy [x y img type health scale])
+(define-struct world [player bullets enemies]) ; Player struct, list of posns, list of enemies structs
+(define player-1 (make-player 200 150 (circle 10 "solid" "blue") 1))
 
 (define (main duration)
-  (big-bang player-1
+  (big-bang (make-world player-1 empty empty) 
             [to-draw show]
             [on-tick tick .1 duration]
             [on-key key-handler]))
 
 (define (show ws)
-  (place-image (scale (player-scale ws) (player-img ws)) (player-x ws) (player-y ws) blank-scene))
+  (place-player ws (place-enemies (world-enemies ws) (place-bullet (world-bullets ws) blank-scene))))
 
-(define (tick ws)
+; place-bullet: List of bullets, image -> Image
+(define (place-bullet lob base)
   (cond
-    [(< (player-y ws) height) (make-player (player-x ws) (+ (player-y ws) (* gravity (player-scale ws))) (player-img ws) (player-scale ws))]
-    [else ws]))
+    [(empty? lob) base]
+    [else (place-image bullet-img (posn-x (first lob)) (posn-y (first lob)) (place-bullet (rest lob) base))]))
 
-(define (jump ws)
-  (make-player (player-x ws) (- (player-y ws) speed) (player-img ws) (player-scale ws)))
+(define (place-enemies loe base)
+  (cond
+    [(empty? loe) base]
+    [else (place-image (enemy-img (first loe)) (enemy-x (first loe)) (enemy-y (first loe)) (place-enemies (rest loe) base))]))
+  
+(define (create-enemy loe)
+  (if (< (/ (random 100) 100) spawn-speed) (add-enemy loe) loe))
 
-(define (move ws direction)
-  (make-player (+ (* speed direction) (player-x ws)) (player-y ws) (player-img ws) (player-scale ws)))
+(define (place-player ws base)
+  (place-image (scale (player-scale (world-player ws)) (player-img (world-player ws))) (player-x (world-player ws)) (player-y (world-player ws)) base))
 
+(define (tick ws) 
+  (make-world (world-player ws) (offscreen-bullets (move-bullets (world-bullets ws))) (offscreen-enemies (move-enemies (create-enemy (world-enemies ws))))))
+
+(define (move-enemies loe)
+  (cond
+    [(empty? loe) empty]
+    [else (cons (make-enemy (enemy-x (first loe)) (+ (enemy-y (first loe)) enemyspeed) (enemy-img (first loe)) (enemy-type (first loe)) (enemy-health (first loe)) (enemy-scale (first loe))) (move-enemies (rest loe)))]))
+
+(define (move-bullets lob)
+  (cond
+    [(empty? lob) empty]
+    [else (cons (make-posn (posn-x (first lob)) (- (posn-y (first lob)) bullet-speed)) (move-bullets (rest lob)))]))
+
+(define (move-player ws direction axis)
+  (if (string=? axis "x") (make-player (+ (* speed direction) (player-x ws)) (player-y ws) (player-img ws) (player-scale ws))
+      (make-player (player-x ws) (+ (* speed direction) (player-y ws)) (player-img ws) (player-scale ws))))
+
+(define (move ws direction axis)
+  (make-world (move-player (world-player ws) direction axis) (world-bullets ws) (world-enemies ws)))
+
+(define (shoot ws)
+  (make-world (world-player ws) (add-bullet ws) (world-enemies ws)))
+
+(define (add-bullet ws)
+  (cons (make-posn (player-x (world-player ws)) (- (player-y (world-player ws)) (* (player-scale (world-player ws)) (/ (image-height (player-img (world-player ws))) 2)))) (world-bullets ws)))
+
+(define (add-enemy loe)
+  (cons (make-enemy (* (random (/ width 10)) 10) -20 enemyimg 1 10 1) loe))
+
+(define (offscreen-enemies loe)
+  (cond
+    [(empty? loe) empty]
+    [(> (enemy-y (first loe)) (+ height (image-height (enemy-img (first loe))))) (offscreen-enemies (rest loe))]
+    [else (cons (first loe) (offscreen-enemies (rest loe)))]))
+
+(define (offscreen-bullets lob)
+  (cond
+    [(empty? lob) empty]
+    [(< (posn-y (first lob)) (- 0 (image-height bullet-img))) (offscreen-bullets (rest lob))]
+    [else (cons (first lob) (offscreen-bullets (rest lob)))]))
+  
 (define (key-handler ws a-key)
   (cond
-    [(key=? "w" a-key) (jump ws)]
-    [(key=? "a" a-key) (move ws -1)]
-    [(key=? "d" a-key) (move ws 1)]
+    [(key=? "w" a-key) (move ws -1 "y")]
+    [(key=? "s" a-key) (move ws 1 "y")]
+    [(key=? "a" a-key) (move ws -1 "x")]
+    [(key=? "d" a-key) (move ws 1 "x")]
+    [(key=? " " a-key) (shoot ws)]
     [else ws]))
 
-(main 100)
+(main 10000000)
